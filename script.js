4008b35d17a8b8248f91afd9d9c7dfcdeb4978b8
alemran22 <alemrangazi@gmail.com>
2024-05-20 20:21:29 +0600

    push

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("button").addEventListener("click", function () {
+    const mainPriceElement = document.getElementById("mainPrice");
+    const mainPriceString = mainPriceElement.innerText;
+    const mainPrice = parseFloat(mainPriceString);
+
+    const inputDiscount = document.getElementById("inputDiscount");
+    const inputDiscountString = inputDiscount.value;
+    const inputDiscountNumber = parseFloat(inputDiscountString);
+
+    const netPriceElement = document.getElementById("netPrice");
+    const netPriceString = netPriceElement.innerText;
+    const netPrice = parseFloat(netPriceString);
+
+    const netTotalDiscountPrice = mainPrice * (inputDiscountNumber / 100);
+    const netTotalPrice = mainPrice - netTotalDiscountPrice;
+    netPriceElement.innerText = netTotalPrice;
+
+});
